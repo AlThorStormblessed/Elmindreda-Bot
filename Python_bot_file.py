d65ebba40027354a46b78518867f78564db70fa3
AlThorStormblessed <86941904+AlThorStormblessed@users.noreply.github.com>
2021-07-05 19:25:46 +0530

    Update Python_bot_file.py

--- Python_bot_file.py
+++ Python_bot_file.py
@@ -21,33 +21,113 @@ def bot_login():
 
 def run_bot(r, comments_replied_to, WholeWord):
     #Keywords that the bot replies to
-    keyword = ["min", "vision", "foretelling", "viewing", "elmindreda", "farshaw", r.user.me()]
+    keyword = ["min", "vision", "foretelling", "viewing", "elmindreda", "farshaw", r.user.me(), "viewings", "visions"]
 
 
     for comment in r.subreddit('Elmindreda_bot').comments(limit = 25):
 
-        for key in keyword:
+        if WholeWord("rand")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            rand_quotes = [
+                "I like older men. I like men with education, and wit. I have no interest in farms, or sheep, or shepherds. But then, you aren't a shepherd, are you? Not anymore. Light, why did the Pattern have to catch me up with you? Why couldn't I have something safe and simple, like being shipwrecked with no food and a dozen hungry Aielmen?",
+                "I'm bound to him as surely as a stave is bound to the barrel. But I can't see if he'll ever love me in return. And I am not the only one.",
+                "You have me, looby.",
+                "Burn you, Rand al'Thor!",
+                "Men are strange. I think it has something to do with the hair on their chins.",
+                "I want to get drunk as a drowned mouse, and fast!",
+                "Fall in love with a man, and you end up doing laundry, even if it does belong to another man."]
 
-            #Searching for keywords and making sure it isn't in the "replied to" list
-            if WholeWord(key)(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            comment.reply(random.choice(rand_quotes)) #Chooses a random quote
 
-                #Opening Quotes.txt, where all the bot quotes are saved
-                with open("Quotes.txt", "r", encoding='cp1252') as f:
-                    quote = f.read()
-                    quote = quote.split("\n")
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
 
-                comment.reply(random.choice(quote)) #Chooses a random quote from Quotes.txt
-
-                comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
-
-                with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
-                    f.write(comment.id + "\n")
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
 
 
-                print("Comment found!")
+            print("Comment found!")
+
+        elif WholeWord("thom")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            thom_quotes = [
+                "Go juggle something, Thom",
+                "Men are strange. I think it has something to do with the hair on their chins."]
+
+            comment.reply(random.choice(thom_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+
+        elif WholeWord("perrin")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            perrin_quotes = [
+                "If you meet a woman – the most beautiful woman you’ve ever seen – run!",
+                "Men are strange. I think it has something to do with the hair on their chins."]
+
+            comment.reply(random.choice(perrin_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+
+        elif WholeWord("toh")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            toh_quotes = [
+                "I don’t know what anybody’s toes have to do with anything, or feet either, but I’m not going anywhere until you talk to them, Rand!"]
+
+            comment.reply(random.choice(toh_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+
+        elif WholeWord("egwene")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            egwene_quotes = [
+                "I am no novice. Yes, Aes Sedai. No, Aes Sedai. May I sweep another floor, Aes Sedai?",
+                ]
+
+            comment.reply(random.choice(egwene_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+
+        else:
+            for key in keyword:
+
+                #Searching for keywords and making sure it isn't in the "replied to" list
+                if WholeWord(key)(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+
+                    #Opening Quotes.txt, where all the bot quotes are saved
+                    with open("Quotes.txt", "r", encoding='cp1252') as f:
+                        quote = f.read()
+                        quote = quote.split("\n")
+
+                    comment.reply(random.choice(quote)) #Chooses a random quote from Quotes.txt
+
+                    comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+                    with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                        f.write(comment.id + "\n")
+
+
+                    print("Comment found!")
 
     print("Sleeping for a bit")
-    time.sleep(300)  #Five minute cool-down period
+    time.sleep(120)  #Two minute cool-down period
 
 #Function to save comments replied to
 def get_saved_comments():
