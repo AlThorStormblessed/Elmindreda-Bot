77e3d39eadbb1b689de644d9313210de60c5f050
AlThorStormblessed <86941904+AlThorStormblessed@users.noreply.github.com>
2021-07-05 15:25:25 +0530

    Update Python_bot_file.py

--- Python_bot_file.py
+++ Python_bot_file.py
@@ -22,17 +22,17 @@ def bot_login():
 def run_bot(r, comments_replied_to, WholeWord):
     #Keywords that the bot replies to
     keyword = ["min", "vision", "foretelling", "viewing", "elmindreda", "farshaw", r.user.me()]
-    
-    
-    for comment in r.subreddit('Elmindreda_bot').comments(limit = 25):        
-        
+
+
+    for comment in r.subreddit('Elmindreda_bot').comments(limit = 25):
+
         for key in keyword:
-            
-            #Searching for keywords and making sure it isn't in the "replied to" list    
+
+            #Searching for keywords and making sure it isn't in the "replied to" list
             if WholeWord(key)(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
-                
+
                 #Opening Quotes.txt, where all the bot quotes are saved
-                with open("Quotes.txt", "r") as f:
+                with open("Quotes.txt", "r", encoding='cp1252') as f:
                     quote = f.read()
                     quote = quote.split("\n")
 
@@ -40,30 +40,30 @@ def run_bot(r, comments_replied_to, WholeWord):
 
                 comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
 
-                with open("Comments_replied_to.txt", "a") as f:           
+                with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
                     f.write(comment.id + "\n")
-                    
-                    
+
+
                 print("Comment found!")
 
     print("Sleeping for a bit")
-    time.sleep(10)  #Ten second cool-down period
-    
+    time.sleep(300)  #Five minute cool-down period
+
 #Function to save comments replied to
 def get_saved_comments():
-    
+
     if not os.path.isfile("comments_replied_to.txt"):
-        
+
         comments_replied_to = []
-        
+
     else:
-        
-        with open("Comments_replied_to.txt", "r") as f:
+
+        with open("Comments_replied_to.txt", "r", encoding='cp1252') as f:
             comments_replied_to = f.read()
             comments_replied_to = comments_replied_to.split("\n")
 
     return comments_replied_to
-            
+
 #Splits comment replied to into whole words
 def WholeWord(w):
 
