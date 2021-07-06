08a61527ce83ffd89f3b51cd8da0248edf42839d
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2021-07-06 10:16:44 -0700

    Add 3 rand's lovers block, add mat block
    add some quote options for various characters
    add special case of rand + perrin

--- Python_bot_file.py
+++ Python_bot_file.py
@@ -33,11 +33,14 @@ def run_bot(r, comments_replied_to, WholeWord):
                 "You have me, looby.",
                 "Burn you, Rand al'Thor!",
                 "Woolheaded sheepherder!",
-                "*Boink*",
+                "*Boink*",                      #love this
                 "Men are strange. I think it has something to do with the hair on their chins.",
                 "I want to get drunk as a drowned mouse, and fast!",
                 "Fall in love with a man, and you end up doing laundry, even if it does belong to another man.",
-                "Rand, don't look at me like that. I am on your side, if it comes to sides. It might; a little. They think I'll tell them what you say. I won't, Rand. They just want to know how to deal with you, what to expect, but I'll not tell one word you don't want me to, and if you ask me to lie, I will. They do not know about my viewings. Those are yours, Rand. You know I will read anyone you say, including Merana and the rest."]
+                "Rand, don't look at me like that. I am on your side, if it comes to sides. It might; a little. They think I'll tell them what you say. I won't, Rand. They just want to know how to deal with you, what to expect, but I'll not tell one word you don't want me to, and if you ask me to lie, I will. They do not know about my viewings. Those are yours, Rand. You know I will read anyone you say, including Merana and the rest.",
+                "He would almost certainly fail without a woman who was dead and gone...",
+                "Fireflies consumed in darkness."
+            ]
 
             comment.reply(random.choice(rand_quotes)) #Chooses a random quote
 
@@ -48,6 +51,45 @@ def run_bot(r, comments_replied_to, WholeWord):
 
 
             print("Comment found!")
+            
+ #perrin
+        elif WholeWord("perrin")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            perrin_quotes = [
+                "If you meet a woman – the most beautiful woman you’ve ever seen – run!",
+                "Men are strange. I think it has something to do with the hair on their chins.",
+                "A wolf, and a broken crown, and trees flowering all around Perrin.",
+                "An Aielman in a cage. A Tuatha'an with a sword. A falcon and a hawk, perching on your shoulders. Both female, I think",
+                "He's found his falcon, and I wouldn't be surprised if she kills him when the hawk appears."
+                ]
+
+            comment.reply(random.choice(perrin_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+            
+ #mat
+        elif WholeWord("mat")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            mat_quotes = [
+                "A red eagle, an eye on a balance scale, a dagger with a ruby, a horn, and a laughing face.",
+                "Men are strange. I think it has something to do with the hair on their chins.",
+                
+                ]
+
+            comment.reply(random.choice(mat_quotes)) #Chooses a random quote
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+            
 #special case of all 3 boys being mentioned at once.
          elif WholeWord("rand")(comment.body) and WholeWord("perrin")(comment.body) and WholeWord("mat")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             three_amigos_quotes = [
@@ -63,11 +105,29 @@ def run_bot(r, comments_replied_to, WholeWord):
 
             print("Comment found!")
 
+#special case of Rand and Perrin mentioned at once.
+         elif WholeWord("rand")(comment.body) and WholeWord("perrin")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            wolfAndDragon_quotes = [
+                "When you two were together, I saw those fireflies and the darkness stronger than ever... But with the two of you in the same room, the fireflies were holding their own instead of being eaten faster than they can swarm, the way they do when you're alone. And there's something else I saw when you two were together. Twice he's going to have to be there, or you... If he's not, something bad will happen to you. Very bad. It will happen if he is not there, but nothing I saw said it won't because he is. It will be very bad, Rand."
+            ]
+            comment.reply(random.choice(wolfAndDragon_quotes)) #Chooses a random quote from above list
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")
+
+
 #thom            
         elif WholeWord("thom")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             thom_quotes = [
                 "Go juggle something, Thom",
-                "Men are strange. I think it has something to do with the hair on their chins."]
+                "Men are strange. I think it has something to do with the hair on their chins.",
+                "A man — not him — juggling fire, and the White Tower."
+                ]
 
             comment.reply(random.choice(thom_quotes)) #Chooses a random quote
 
@@ -79,21 +139,7 @@ def run_bot(r, comments_replied_to, WholeWord):
 
             print("Comment found!")
 
-        elif WholeWord("perrin")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
-            perrin_quotes = [
-                "If you meet a woman – the most beautiful woman you’ve ever seen – run!",
-                "Men are strange. I think it has something to do with the hair on their chins."]
-
-            comment.reply(random.choice(perrin_quotes)) #Chooses a random quote
-
-            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
-
-            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
-                f.write(comment.id + "\n")
-
-
-            print("Comment found!")
-
+#toh
         elif WholeWord("toh")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             toh_quotes = [
                 "I don’t know what anybody’s toes have to do with anything, or feet either, but I’m not going anywhere until you talk to them, Rand!"]
@@ -112,6 +158,7 @@ def run_bot(r, comments_replied_to, WholeWord):
             gawyn_quotes = [
                 "Gawyn, you know me. You can't think I would help the Black Ajah. Gawyn, Elayne supports her and everything she's done. Your own sister, Gawyn. Egwene believe in her too, Gawyn. I swear it, Gawyn. Egwene believes.",
                 "I fear I know little of books my Lord Gawyn. I always mean to read one – I do. But there is so little time. Why, just fixing my hair properly takes hours. Do you think it is pretty?",
+                "Streaks of dried blood had made his face a grim mask. He was going to be wounded on the day the Aes Sedai died. He was going to be hurt more than the blood told, hurt somehow deeper than wounds to his flesh...that bloody mask again. More: a sword floated above his head, and a banner waved behind it. The long-hilted sword, like those most Warders used, had a heron engraved on its slightly curved blade, symbol of a blademaster, and Min could not say whether it belonged to Gawyn or threatened him. The banner bore Gawyn's sigil of the charging White Boar, but on a field of green rather than the red of Andor."
                 ]
 
             comment.reply(random.choice(gawyn_quotes)) #Chooses a random quote
@@ -123,7 +170,25 @@ def run_bot(r, comments_replied_to, WholeWord):
 
 
             print("Comment found!")
-  #egwene          
+            
+ #special case of all 3 rand-y ones being mentioned at once. (Min, Elayne, Aviendha) Min is represented by 'min' as well as 'you' (since the bot is Min)
+         elif WholeWord("you")(comment.body) and WholeWord("elayne")(comment.body) and WholeWord("aviendha")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me() or WholeWord("min")(comment.body) and WholeWord("elayne")(comment.body) and WholeWord("aviendha")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
+            three_randyones_quotes = [
+                "Two. Two others. And....And I'm one.",
+                "Three women before a pyre."
+                ]
+
+            comment.reply(random.choice(three_randyones_quotes)) #Chooses a random quote from above list
+
+            comments_replied_to.append(comment.id)  #Simply adds the comment.id replied to to a list so the bot doesn't reply to it again
+
+            with open("Comments_replied_to.txt", "a", encoding='cp1252') as f:
+                f.write(comment.id + "\n")
+
+
+            print("Comment found!")    
+            
+ #egwene          
         elif WholeWord("egwene")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             egwene_quotes = [
                 "I am no novice. Yes, Aes Sedai. No, Aes Sedai. May I sweep another floor, Aes Sedai?",
@@ -140,7 +205,7 @@ def run_bot(r, comments_replied_to, WholeWord):
 
 
             print("Comment found!")
-   #aviendha        
+ #aviendha        
         elif WholeWord("aviendha")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             aviendha_quotes = [
                 "I don’t know what anybody’s toes have to do with anything, or feet either, but I’m not going anywhere until you talk to them, Rand!",
@@ -159,7 +224,8 @@ def run_bot(r, comments_replied_to, WholeWord):
   #elayne
         elif WholeWord("elayne")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             elayne_quotes = [
-                "She will have to share her husband with two other women and she will be a queen. A severed hand, not hers."
+                "She will have to share her husband with two other women and she will be a queen. A severed hand, not hers.",
+                "... and above Elayne's, a red hot iron and an axe."
                 ]
 
             comment.reply(random.choice(elayne_quotes)) #Chooses a random quote
@@ -174,7 +240,9 @@ def run_bot(r, comments_replied_to, WholeWord):
 #nynaeve
         elif WholeWord("nynaeve")(comment.body) and comment.id not in comments_replied_to and not comment.author == r.user.me():
             nynaeve_quotes = [
-                "She's part of it, right along with the rest of you...The sparks, Rand. She met Moiraine coming in, and there were sparks, with just the two of them. Yesterday I couldn't see sparks without at least three or four of you together, but today it's all sharper, and more furious. You're all in more danger today than yesterday. Since she came."
+                "She's part of it, right along with the rest of you...The sparks, Rand. She met Moiraine coming in, and there were sparks, with just the two of them. Yesterday I couldn't see sparks without at least three or four of you together, but today it's all sharper, and more furious. You're all in more danger today than yesterday. Since she came.",
+                "A man's ring of heavy gold floated above Nynaeve's head",
+                "Nynaeve kneeling over someone's corpse in a posture of grief."
                 ]
 
             comment.reply(random.choice(nynaeve_quotes)) #Chooses a random quote
